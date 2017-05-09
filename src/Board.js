5a14f1fd580e69780248a04fcd92c3d8c7cc901e
Ryan Kent <imsolost@gmail.com>
2017-05-09 09:57:33 -0700

    changing board setup

--- src/Board.js
+++ src/Board.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react'
-import Square from './Square'
+// import Square from './Square'
+
+
+class Square {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+    this.piece = null
+  }
+}
 
 class Board extends Component {
   constructor(props) {
     super(props)
+    // this.state = {
+    //   board: Array( 64 ).fill( null )
+    // }
     this.state = {
-      board: Array( 64 ).fill( null )
+      board: this.initializeBoard(),
+      selected: null
     }
   }
 
+  // initializeBoard = () => {
+  //   return this.state.board.map( ( square, index ) =>
+  //     <Square {...square} key={`square-${index}`} index={index}/> )
+  // }
+
+  initializeBoard = () => {
+    let board = {}
+    for (let y = 1; y <= 8; y++) {
+      board[y] = {}
+      for (let x = 1; x <= 8; x++) {
+        board[y][x] = new Square(x, y)
+      }
+    }
+    return board
+  }
+
+
   render() {
+    let boardRender = Object.values(this.state.board).reverse().map( (row, index) => {
+      let rows = Object.values(row).map( square => {
+        if (square.x + square.y % 2 ===1) {
+          return <div key={square.x+square.y} className='white-square'>{'x:'+square.x+' '+'y:'+square.y}</div>
+
+        } else {
+          return <div key={square.x+square.y} className='black-square'>{'x:' + square.x+' '+'y:'+square.y}</div>
+        }
+      })
+      return <div className='row' key={index}>{rows}</div>
+    })
+
     return (
       <div className='board'>
-        { this.state.board.map( ( square, index ) =>
-          <Square {...square} key={`square-${index}`} index={index} /> )}
+        { boardRender }
       </div>
     )
   }
--- src/Square.js
+++ src/Square.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 
 class Square extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      x: 0,
+      y: 0,
+      piece: null
+    }
+  }
 
-  squareColor = (index) => {
+  position = (index) => {
+    return [ Math.floor(index / 8), index % 8 ]
+  }
+
+  determineColor = (index) => {
     if ( (index % 2 === 0 && index < 8) ||
     (index % 2 === 1 && index > 7 && index < 16) ||
     (index % 2 === 0 && index > 15 && index < 23) ||
@@ -11,15 +23,15 @@ class Square extends Component {
     (index % 2 === 1 && index > 39 && index < 48) ||
     (index % 2 === 0 && index > 47 && index < 55) ||
     (index % 2 === 1 && index > 55 && index < 64) ) {
-      return <div className='white-square'>{this.props.index}</div>
+      return <div className='white-square'>{this.position(index)}</div>
     } else {
-        return <div className='black-square'>{this.props.index}</div>
+        return <div className='black-square'>{this.position(index)}</div>
     }
   }
 
   render() {
     return (
-      <div>{this.squareColor(this.props.index)}</div>
+      <div>{this.determineColor(this.props.index)}</div>
     )
   }
 }
